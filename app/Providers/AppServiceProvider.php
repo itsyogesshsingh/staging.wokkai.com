a13042ff3c6a86c7400b2f5e7751d00bf8c9ce6f
itsyogesshsingh <yogessh.singh07@gmail.com>
2026-07-21 18:09:42 +0530

    dev/user-mgmt: fix http error (need https)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +21,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // force https in production and staging environments
+        if (app()->environment('staging', 'production')) {
+            URL::forceScheme('https');
+        }
+
         Gate::before(function ($user, $ability) {
             return $user->hasRole('root') ? true : null;
         });
